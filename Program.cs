99d72d720371a58c0b4d390c04d873dd08e6cc58
Alexandr <milan.801813@gmail.com>
2023-02-08 11:30:48 +0300

    Задание

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите элементы массива через пробел ");
+string text = Console.ReadLine();
+if (text == "")
+{
+    Console.WriteLine("введенный массив пустой");
+    Environment.Exit(0);
+}
+var inputArray = text.Split(' ');
+string[] arrayNew = new string[inputArray.Length];
+int j = 0;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        arrayNew[j] = inputArray[i];
+        j++;
+    }
+}
+Array.Resize(ref arrayNew, j);
+Console.WriteLine("полученный массив : " + string.Join(",", arrayNew));
